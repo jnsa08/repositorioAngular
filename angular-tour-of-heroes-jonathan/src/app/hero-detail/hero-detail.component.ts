8fe92ed4cb1fe2df2f5faf4f918540881d4e9ee7
Jonathan Saavedra <jsaavedra@optimisa.cl>
2018-05-24 15:55:10 -0400

    Se añade routing y message

--- angular-tour-of-heroes-jonathan/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes-jonathan/src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
 
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService }  from '../hero.service';
+
 
 @Component({
   selector: 'app-hero-detail',
@@ -11,9 +16,22 @@ export class HeroDetailComponent implements OnInit {
 
   @Input() heroDetail: Hero;
 
-  constructor() { }
+  constructor(  private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location) { }
 
   ngOnInit() {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id)
+      .subscribe(hero => this.heroDetail = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
